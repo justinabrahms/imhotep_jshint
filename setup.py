a885bf737ecb4c0c19fcb2f1282a6abf4d2bc08a
Justin Abrahms <justin@abrah.ms>
2014-07-01 14:46:01 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep_jshint',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep_jshint',
     license='MIT',
