991f526a09188928c36756062700dfcc778804b4
Justin Abrahms <justin@abrah.ms>
2014-07-01 14:45:09 -0700

    Fix poor string matching, which caused errors.

--- imhotep_jshint/plugin.py
+++ imhotep_jshint/plugin.py
@@ -3,11 +3,12 @@ import os
 import re
 
 class JSHint(Tool):
-    response_format = re.compile(r'^(?P<filename>.*): " \
-        "line (?P<line_number>\d+), col \d+, (?P<message>.*)$')
+    response_format = re.compile(r'^(?P<filename>.*): ' \
+        r'line (?P<line_number>\d+), col \d+, (?P<message>.*)$')
     jshintrc_filename = '.jshintrc'
 
     def process_line(self, dirname, line):
+        "myapp.js: line 27, col 14, you messed up"
         line = line[len(dirname) + 1:]  # +1 for trailing slash to make relative
         match = self.response_format.search(line)
         if match is not None:
